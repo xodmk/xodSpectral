5b62589a7dca304a8cad8aa0d96f31c4126b65d8
eschei <djotoxodmk@gmail.com>
2023-08-17 15:49:58 +0900

    Updated vocoder_tb

--- xodVocoder_tb.py
+++ xodVocoder_tb.py
@@ -9,10 +9,7 @@
 # ___::((XODMK Programming Industries))::___
 # ___::((XODMK:CGBW:BarutanBreaks:djoto:2023))::___
 #
-#
-# ODMK Phase Vocoder
-#
-# Using Librosa vocoder implementation for model
+# XODMK Phase Vocoder
 #
 # *****************************************************************************
 # /////////////////////////////////////////////////////////////////////////////
@@ -37,8 +34,11 @@ print("audioOutDir: " + audioOutDir)
 sys.path.insert(0, rootDir + '/xodma')
 
 from xodmaAudioTools import load_wav, write_wav
+from xodmaSpectralTools import amplitude_to_db, stft, istft, magphase, peak_pick
 from xodmaVocoder import pvTimeStretch, pvPitchShift, pvRobotStretch
 from xodmaSpectralPlot import specshow
+from xodmaMiscUtil import valid_audio
+
 
 sys.path.insert(1, rootDir + '/xodUtil')
 import xodPlotUtil as xodplt
@@ -48,7 +48,7 @@ sys.path.insert(2, rootDir + '/xodDSP')
 import xodClocks as clks
 import xodWavGen as wavGen
 
-# temp python debugger - use >>>pdb.set_trace() to set break
+# temp python debugger - use >>> pdb.set_trace() to set break
 import pdb
 
 # // *---------------------------------------------------------------------* //
@@ -61,10 +61,6 @@ plt.close('all')
 # begin : function definitions
 # #############################################################################
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-# // *---------------------------------------------------------------------* //
-
 
 def arrayFromFile(fname):
     """ reads .dat data into Numpy array:
@@ -90,6 +86,30 @@ def arrayFromFile(fname):
     return arrayNm
 
 
+def rolling_rms(x, winSz):
+    # prepend zeros so rolling average result aligns with original time series
+    zeroPadX = np.pad(x, (winSz, 0), mode='constant', constant_values=0)
+    xc = np.cumsum(zeroPadX ** 2)
+    return np.sqrt((xc[winSz:] - xc[:-winSz]) / winSz)
+
+
+def plot_rolling_rms(x, winSz, plotNum, xName):
+    rrms = rolling_rms(x, winSz)
+    # pdb.set_trace()
+
+    pltTitle = 'Rolling RMS: ' + xName
+    pltXlabel = 'time-domain wav'
+    pltYlabel = 'RMS Magnitude'
+
+    # define a linear space from 0 to 1/2 Fs for x-axis:
+    rraxis = np.linspace(0, len(rrms), len(rrms))
+    # xodplt.xodPlot1D(plotNum, rrms, rraxis, pltTitle, pltXlabel, pltYlabel)
+    # xodplt.xodPlot1D(plotNum, x, rraxis, pltTitle, pltXlabel, pltYlabel)
+
+    rrmsArray = np.transpose(np.column_stack((x, rrms)))
+    xodplt.xodMultiPlot1D(plotNum, rrmsArray, rraxis, pltTitle, pltXlabel, pltYlabel)
+
+
 print('// //////////////////////////////////////////////////////////////// //')
 print('// *--------------------------------------------------------------* //')
 print('// *---::XODMK Spectral Tools Experiments::---*')
@@ -100,32 +120,38 @@ print('// //////////////////////////////////////////////////////////////// //')
 # // *--User Settings - Primary parameters--*
 # // *---------------------------------------------------------------------* //
 
+# Algorithm Select:
+
+runAudioSTFT = 1        # Run EXP STFT <-> iSTFT
+runVocoderPV1 = 1       # Run EXP using PV1 phase vocoder algorithm
+runRobotSmith = 1       # Run EXP using pvRobotSmith phase vocoder algorithm
+
+runPlots = 1            # Run Plotting
+
+
 # srcSel: 0 = wavSrc, 1 = amenBreak, 2 = sineWave48K, 
 #         3 = multiSin test, 4 = text array input
 
 srcSel = 0
 
-# STEREO source signal
-# wavSrc = 'dsvco.wav'
-# wavSrc = 'detectiveOctoSpace_one.wav'
-# wavSrc = 'ebolaCallibriscian_uCCrhythm.wav'
-# wavSrc = 'zoroastorian_mdychaos1.wav'
-# wavSrc = 'The_Amen_Break_odmk.wav'
 
 # MONO source signal
 # wavSrc = 'multiSinOut48KHz_1K_3K_5K_7K_9K_16sec.wav'
 
-wavSrcA = 'jahniBoyQuestaVox11.wav'
-# wavSrcA = 'opium_house_1.wav'
-# wavSrcB = 'scoolreaktor_beatx03.wav'
-# wavSrcB = 'gorgulans_beatx01.wav'
+# STEREO source signal
+wavSrc = 'jahniBoyQuestaVox11.wav'
+# wavSrc = 'opium_house_1.wav'
+# wavSrc = 'dsvco.wav'
+# wavSrc = 'detectiveOctoSpace_one.wav'
+# wavSrc = 'ebolaCallibriscian_uCCrhythm.wav'
+
 
 # length of input signal:
 # '0'   => full length of input .wav file
 # '###' => usr defined length in SECONDS
 wavLength = 0
 
-NFFT = 2048
+NFFT = 1024
 STFTHOP = int(NFFT / 4)
 WIN = 'hann'
 
@@ -154,50 +180,43 @@ tukey (needs taper fraction)
 
 '''
 
-# // *---------------------------------------------------------------------* //
-# // *---------------------------------------------------------------------* //
+# define plotting fnum -> increment for each plot
+fnum = 0
 
+# // *---------------------------------------------------------------------* //
+# // *----- Load .wav file -----*
+# // *---------------------------------------------------------------------* //
 
 # inputs:  wavIn, audioSrcDir, wavLength
 # outputs: ySrc_ch1, ySrc_ch2, numChannels, fs, ySamples
 
-
 # Load Stereo/mono .wav file
 
-
 if srcSel == 0:
-    srcANm = wavSrcA
+    srcNm = wavSrc
 elif srcSel == 1:
-    srcANm = 'The_Amen_Break_48K.wav'
+    srcNm = 'The_Amen_Break_48K.wav'
 elif srcSel == 2:
-    srcANm = 'MonoSinOut_48K_560Hz_5p6sec.wav'
+    srcNm = 'MonoSinOut_48K_560Hz_5p6sec.wav'
 elif srcSel == 3:
-    srcANm = 'multiSinOut48KHz_1K_3K_5K_7K_9K_16sec.wav'
+    srcNm = 'multiSinOut48KHz_1K_3K_5K_7K_9K_16sec.wav'
 
-audioSrcA = audioSrcDir + '/' + srcANm
+audioSrc = audioSrcDir + '/' + srcNm
 
-# audioSrcB = audioSrcDir+wavSrcB
+[aSrc, aNumChannels, afs, aLength, aSamples] = load_wav(audioSrc, wavLength, True)
 
-
-[aSrc, aNumChannels, afs, aLength, aSamples] = load_wav(audioSrcA, wavLength, True)
-
-# [bSrc, bNumChannels, bfs, bLength, bSamples] = load_wav(audioSrcB, wavLength, True)
-
+# Expected return variable properties:
+# >> aSrc.ndim = 2
+# >> aSrc.dtype = ('float64')
+# >> aSrc.shape = (xLength, 2)
+# >> isinstance(aSrc, np.ndarray) = True
 
 if aNumChannels == 2:
     aSrc_ch1 = aSrc[:, 0]
     aSrc_ch2 = aSrc[:, 1]
 else:
     aSrc_ch1 = aSrc
-    aSrc_ch2 = 0
-
-# if bNumChannels == 2:
-#    bSrc_ch1 = bSrc[:,0];
-#    bSrc_ch2 = bSrc[:,1];
-# else:
-#    bSrc_ch1 = bSrc;
-#    bSrc_ch2 = 0;
-
+    aSrc_ch2 = aSrc
 
 # aT = 1.0 / afs
 # print('\nsample period: ------------------------- '+str(aT))
@@ -206,137 +225,156 @@ else:
 
 # // *--- Plot - source signal ---*
 
-if 1:
-    fnum = 3
-    pltTitle = 'Input Signals: aSrc_ch1'
-    pltXlabel = 'sinArray time-domain wav'
+if runPlots:
+    fnum += 1
+    pltTitle = 'Input Waveform: ' + srcNm
+    pltXlabel = 'time-domain wav'
     pltYlabel = 'Magnitude'
 
     # define a linear space from 0 to 1/2 Fs for x-axis:
     xaxis = np.linspace(0, len(aSrc_ch1), len(aSrc_ch1))
-
     xodplt.xodPlot1D(fnum, aSrc_ch1, xaxis, pltTitle, pltXlabel, pltYlabel)
 
-    plt.show()
-
-# pdb.set_trace()
-
 # // *---------------------------------------------------------------------* //
 # // *---------------------------------------------------------------------* //
 
-if 0:
+if runAudioSTFT:
+    print('\n')
+    print('// *---:: Audio STFT <-> iSTFT test ::---*')
+
+    n_fft = NFFT
+    aSrcSTFT = stft(aSrc_ch1, n_fft=n_fft)
+    aSrcInverse = istft(aSrcSTFT, dtype=aSrcSTFT.dtype)
+
+    if runPlots:
+        # fnum += 1
+        # pltTitle = 'Inverse STFT: aSrcInverse'
+        # pltXlabel = 'time-domain wav'
+        # pltYlabel = 'Magnitude'
+        #
+        # # define a linear space from 0 to 1/2 Fs for x-axis:
+        # xaxis = np.linspace(0, len(aSrcInverse), len(aSrcInverse))
+        # xodplt.xodPlot1D(fnum, aSrcInverse, xaxis, pltTitle, pltXlabel, pltYlabel)
+
+        # Plot composite wav forms
+        shape1 = aSrc_ch1.shape[0]
+        shape2 = aSrcInverse.shape[0]
+        if shape1 > shape2:
+            # Pad array2 with zeros
+            array1 = aSrc_ch1
+            array2 = np.pad(aSrcInverse, (0, shape1 - shape2), mode='constant', constant_values=0)
+        elif shape2 > shape1:
+            # Pad array1 with zeros
+            array1 = np.pad(aSrc_ch1, (0, shape2 - shape1), mode='constant', constant_values=0)
+            array2 = aSrcInverse
+        errArray = array1 - array2
+        sigArray = np.transpose(np.column_stack((array1, array1, errArray)))
+
+        fnum += 1
+        pltTitle = 'Wave Source vs. Inverse STFT'
+        pltXlabel = 'time-domain wav'
+        pltYlabel = 'Magnitude'
+        # define a linear space from 0 to 1/2 Fs for x-axis:
+        xaxis = np.linspace(0, len(aSrcInverse), len(aSrcInverse))
+        xodplt.xodMultiPlot1D(fnum, sigArray, xaxis, pltTitle, pltXlabel, pltYlabel)
+
+        fnum += 1
+        rrmsWinSize = STFTHOP
+        rmsName = 'wavSrc_ch1'
+        plot_rolling_rms(aSrc_ch1, rrmsWinSize, fnum, rmsName)
+
+# // *---------------------------------------------------------------------* //
+
+if runVocoderPV1:
     print('\n')
     print('// *---:: Phase Vocoder Time-Stretch EFX test ::---*')
 
-    '''
-    Compress to be twice as fast
+    n_fft = NFFT
 
-    >>> y, sr = librosa.load(librosa.util.example_audio_file())
-    >>> y_fast = librosa.effects.time_stretch(y, 2.0)
+    timeCompress = 1.33     # Time Compress rate
+    timeExpand = 0.56       # Time Expand rate
 
-    Or half the original speed
+    yPV1_Compress_ch1 = pvTimeStretch(aSrc_ch1, timeCompress, n_fft)
+    yPV1_Compress_ch2 = pvTimeStretch(aSrc_ch2, timeCompress, n_fft)
 
-    >>> y_slow = librosa.effects.time_stretch(y, 0.5) '''
+    yPV1_Compress = np.transpose(np.column_stack((yPV1_Compress_ch1, yPV1_Compress_ch2)))
 
-    # Time Compress rate:
-    R = 1.23
+    print('\nPerformed Time-Compress by ' + str(1/timeCompress))
 
-    # Time Expand rate:
-    S = 0.3
+    yPV1_Expand_ch1 = pvTimeStretch(aSrc_ch1, timeExpand, n_fft)
+    yPV1_Expand_ch2 = pvTimeStretch(aSrc_ch2, timeExpand, n_fft)
 
-    yRxFast_ch1 = pvTimeStretch(aSrc_ch1, 2.0)
-    yRxFast_ch2 = pvTimeStretch(aSrc_ch2, 2.0)
+    yPV1_Expand = np.transpose(np.column_stack((yPV1_Expand_ch1, yPV1_Expand_ch2)))
 
-    yRxFast = np.transpose(np.column_stack((yRxFast_ch1, yRxFast_ch2)))
-
-    print('\nPerformed time_stretch by R (Rxfast)')
-
-    # pdb.set_trace()
-
-    ySxSlow_ch1 = pvTimeStretch(aSrc_ch1, 0.5)
-    ySxSlow_ch2 = pvTimeStretch(aSrc_ch2, 0.5)
-
-    ySxSlow = np.transpose(np.column_stack((ySxSlow_ch1, ySxSlow_ch2)))
-
-    print('\nPerformed time_stretch by S (Sxslow)')
+    print('Performed Time-Expand by ' + str(1/timeExpand))
 
     print('\n// *---:: Write .wav files ::---*')
 
-    outFilePath = audioOutDir + '/yOriginal.wav'
+    outFilePath = audioOutDir + '/yPV1_Original.wav'
     write_wav(outFilePath, aSrc, afs)
 
-    outFilePath = audioOutDir + '/yRxFast.wav'
-    write_wav(outFilePath, yRxFast, afs)
+    outFilePath = audioOutDir + '/yPV1_Compress.wav'
+    write_wav(outFilePath, yPV1_Compress, afs)
 
-    outFilePath = audioOutDir + '/ySxSlow.wav'
-    write_wav(outFilePath, ySxSlow, afs)
+    outFilePath = audioOutDir + '/yPV1_Expand.wav'
+    write_wav(outFilePath, yPV1_Expand, afs)
 
-    print('\n\nOutput directory: ' + audioOutDir)
-    print('\nwrote .wav file yOriginal.wav')
-    print('\nwrote .wav file yRxFast.wav')
-    print('\nwrote .wav file ySxSlow.wav')
+    print('\nOutput directory: ' + audioOutDir)
+    print('wrote .wav file yPV1_Original.wav')
+    print('wrote .wav file yPV1_Compress.wav')
+    print('wrote .wav file yPV1_Expand.wav')
 
 
-if 1:
+# // *---------------------------------------------------------------------* //
+
+if runRobotSmith:
     print('\n')
     print('// *---:: Phase Vocoder RobotSmith EFX test ::---*')
 
-    '''
-    Compress to be twice as fast
+    n_fft = NFFT
 
-    >>> y, sr = librosa.load(librosa.util.example_audio_file())
-    >>> y_fast = librosa.effects.time_stretch(y, 2.0)
-
-    Or half the original speed
-
-    >>> y_slow = librosa.effects.time_stretch(y, 0.5) '''
-
-    # Time Compress rate:
-    timeCompress = 1.33
-
-    # Time Expand rate:
-    timeExpand = 0.56
+    timeCompress = 1.33     # Time Compress rate
+    timeExpand = 0.56       # Time Expand rate
 
     # Vox Modulation Depth - range[0.01, 1.0] :
-    vxmod = 0.05
+    # vxmod = 0.0
+    vxmod = 0.015
+    # vxmod = 0.55
+    # vxmod = 0.1
+    # vxmod = 6.25
 
     # Vox Mod Stereo
-    vxtilt = -0.5
+    vxtilt = -1.0   # -1 = tilt left, 1.0 = tilt right
+    vxmodL = (vxtilt * vxmod)
+    vxmodR = -(vxtilt * vxmod)
 
-    vxmodL = vxmod - (vxtilt * vxmod)
-    vxmodR = vxmod + (vxtilt * vxmod)
+    yRS_Compress_ch1 = pvRobotStretch(aSrc_ch1, timeCompress, vxmodL, n_fft)
+    yRS_Compress_ch2 = pvRobotStretch(aSrc_ch2, timeCompress, vxmodR, n_fft)
+    yRS_Compress = np.transpose(np.column_stack((yRS_Compress_ch1, yRS_Compress_ch2)))
 
-    yRxFast_ch1 = pvRobotStretch(aSrc_ch1, timeCompress, vxmodL)
-    yRxFast_ch2 = pvRobotStretch(aSrc_ch2, timeCompress, vxmodR)
+    print('\nPerformed Time-Compress by ' + str(1/timeCompress))
 
-    yRxFast = np.transpose(np.column_stack((yRxFast_ch1, yRxFast_ch2)))
+    yRS_Expand_ch1 = pvRobotStretch(aSrc_ch1, timeExpand, vxmodL, n_fft)
+    yRS_Expand_ch2 = pvRobotStretch(aSrc_ch2, timeExpand, vxmodR, n_fft)
+    yRS_Expand = np.transpose(np.column_stack((yRS_Expand_ch1, yRS_Expand_ch2)))
 
-    print('\nPerformed time_compress by ' + str(1/timeCompress))
-
-    # pdb.set_trace()
-
-    ySxSlow_ch1 = pvRobotStretch(aSrc_ch1, timeExpand, vxmodL)
-    ySxSlow_ch2 = pvRobotStretch(aSrc_ch2, timeExpand, vxmodR)
-
-    ySxSlow = np.transpose(np.column_stack((ySxSlow_ch1, ySxSlow_ch2)))
-
-    print('\nPerformed time_stretch by ' + str(1/timeExpand))
+    print('Performed Time-Expand by ' + str(1/timeExpand))
 
     print('\n// *---:: Write .wav files ::---*')
 
-    outFilePath = audioOutDir + '/yOriginal.wav'
+    outFilePath = audioOutDir + '/yRS_Original.wav'
     write_wav(outFilePath, aSrc, afs)
 
-    outFilePath = audioOutDir + '/yRxFast.wav'
-    write_wav(outFilePath, yRxFast, afs)
+    outFilePath = audioOutDir + '/yRS_Compress.wav'
+    write_wav(outFilePath, yRS_Compress, afs)
 
-    outFilePath = audioOutDir + '/ySxSlow.wav'
-    write_wav(outFilePath, ySxSlow, afs)
+    outFilePath = audioOutDir + '/yRS_Expand.wav'
+    write_wav(outFilePath, yRS_Expand, afs)
 
-    print('\n\nOutput directory: ' + audioOutDir)
-    print('\nwrote .wav file yOriginal.wav')
-    print('\nwrote .wav file yRxFast.wav')
-    print('\nwrote .wav file ySxSlow.wav')
+    print('\nOutput directory: ' + audioOutDir)
+    print('wrote .wav file yRS_Original.wav')
+    print('wrote .wav file yRS_Compress.wav')
+    print('wrote .wav file yRS_Expand.wav')
 
 
 # // *---------------------------------------------------------------------* //
@@ -371,7 +409,7 @@ print('// *--------------------------------------------------------------* //')
 #
 # for(posin=posout=0; posin < input_size; posin+=hopsize){
 #      mod = posin%fftsize;
-#	// window & rotate a signal frame
+#	   # window & rotate a signal frame
 #      for(i=0; i < fftsize; i++) 
 #          if(posin+i < input_size)
 #            sigframe[(i+mod)%fftsize]
@@ -398,7 +436,7 @@ print('// *--------------------------------------------------------------* //')
 #
 #      // construct the amplitude-frequency pairs
 #      specframe[i] = mag;
-#	  specframe[i+1] = (delta + k*scal)*fac;
+#	  specframe[i+1] = (delta + k*scal) * fac;
 #
 #      }
 #      // output it
